cca7c932f63125930d9109ea14266fc1c7422d37
Sharifzoda Iqbol <iqbolsharifzoda724@gmail.com>
2024-02-14 19:07:57 +0300

    Update linked_list.py

--- linked_list.py
+++ linked_list.py
@@ -1,3 +1,7 @@
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
 class LinkedList:
     def __init__(self):
         self.head = None
@@ -25,13 +29,13 @@ class LinkedList:
 
     def remove_first(self):
         if not self.head:
-            raise ValueError('Linked list is empty')
+            raise ValueError('Связанный список пуст')
         self.head = self.head.next
         self.length -= 1
 
     def remove_last(self):
         if not self.head:
-            raise ValueError('Linked list is empty')
+            raise ValueError('Связанный список пуст')
         if not self.head.next:
             self.head = None
         else:
@@ -43,7 +47,7 @@ class LinkedList:
 
     def remove_at(self, index):
         if index < 0 or index >= self.length:
-            raise ValueError('Invalid index')
+            raise ValueError('Недопустимый индекс')
         if index == 0:
             self.head = self.head.next
         else:
@@ -55,7 +59,7 @@ class LinkedList:
 
     def remove_first_value(self, value):
         if not self.head:
-            raise ValueError('Linked list is empty')
+            raise ValueError('Связанный список пуст')
         
         if self.head.data == value:
             self.head = self.head.next
@@ -69,11 +73,11 @@ class LinkedList:
                 self.length -= 1
                 return
             current = current.next
-        raise ValueError('Value not found')
+        raise ValueError('Значение не найдено')
 
     def remove_last_value(self, value):
         if not self.head:
-            raise ValueError('Linked list is empty')
+            raise ValueError('Связанный список пуст')
         
         if self.head.data == value:
             self.head = self.head.next
@@ -91,11 +95,4 @@ class LinkedList:
                 self.length -= 1
                 return
             current = current.next
-        raise ValueError('Value not found')
-
-
-class Node:
-    def __init__(self, data=None):
-        self.data = data
-        self.next = None
-        
+        raise ValueError('Значение не найдено')
